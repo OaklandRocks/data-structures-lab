b952163f3bd1249b581605618dcd1ad5634426cd
Peter Brown <peterbrownpa@gmail.com>
2017-03-28 13:08:00 -0700

    set solution

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  if(this._storage.indexOF(target) > -1){
+    return true;
+  }
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
