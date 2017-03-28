ebe73e4f6eddf8167ead6e6d262728c745b419a6
Peter Brown <peterbrownpa@gmail.com>
2017-03-28 12:36:50 -0700

    prototypal style

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+
+  queue.length = 0;
+  queue.beginning = 1;
+  queue.storage = {};
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.length ++;
+    this.beginning ++;
+    this.storage[this.length] = value;
+  },
+
+  dequeue: function(){
+    if (this.length > 0 {
+      this.length --;
+      this.beginning++;
+    }
+    return this.storage[this.beginning -1]
+
+  },
+  size: function(){
+    return this.length
+  }
+
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods)
+  stack.length =0;
+  stack.storage = {};
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.length++;
+    this.storage[this.length]=value;
+  },
+  pop: function(){
+    return this.length;
+  }
+};
 
 
