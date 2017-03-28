771aab10d77291f7fa733426393188fbe9ddd47b
Peter Brown <peterbrownpa@gmail.com>
2017-03-28 13:04:06 -0700

    linkedList kinda done

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+    //if empty
+    if(!list.head){
+      list.head = newTail;
+    }
+    if (list.tail){
+      list.tail.next = newTail;
+    }
+    list.tail = newTail;
   };
 
   list.removeHead = function() {
+    if (list.head === null){
+      return null;
+    }
+    var currentHead = list.head;
+    list.head = list.head.next;
+    return currentHead.value;
   };
 
   list.contains = function(target) {
+    var node = list.head;
+
+    while(node) {
+      if (node.value === target){
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
